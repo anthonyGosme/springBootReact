ea5a98ec99438df0e3e05515e05c7959bf0491b7
AnthonyGosme <anthonygosme@gmail.com>
2020-02-01 22:30:36 +0100

    add h2 database

--- src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
+++ src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
@@ -3,6 +3,7 @@ package com.packt.cardatabase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class CardatabaseApplicationTests {
 
