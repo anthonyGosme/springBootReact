56cf6d94709e5a9cb701196a36da3174c02f01d0
AnthonyGosme <anthonygosme@gmail.com>
2020-02-05 22:43:21 +0100

    add dara rest

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,9 +2,11 @@ package com.packt.cardatabase.domain;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
 
     @Query("select c from Car c where c.brand like  %?1")
