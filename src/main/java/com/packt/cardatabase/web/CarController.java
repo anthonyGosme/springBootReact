d2f1a58391f7660b597500cfe68a83394cb1ebf6
AnthonyGosme <anthonygosme@gmail.com>
2020-02-11 11:07:57 +0100

    improve & refactorize

--- src/main/java/com/packt/cardatabase/web/CarController.java
+++ src/main/java/com/packt/cardatabase/web/CarController.java
@@ -2,16 +2,23 @@ package com.packt.cardatabase.web;
 
 import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
 @RestController
-public class carController {
+public class CarController {
 
-  @Autowired private CarRepository carRepository;
+  private final CarRepository carRepository;
 
-  @RequestMapping("/cars")
+  public CarController(CarRepository carRepository) {
+    this.carRepository = carRepository;
+  }
+
+  @RequestMapping(
+      value = "/cars",
+      method = {GET, POST, DELETE, PATCH})
   public Iterable<Car> getCars() {
     return carRepository.findAll();
   }
